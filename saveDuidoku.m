7fccff71d48aeafa2892e7e2520f19ede0b1c93c
Vasilis Arampatzis <arabatzis.vasileios@gmail.com>
2024-10-07 15:37:07 +0300

    Commit Database

---
+++ saveDuidoku.m
@@ -0,0 +1,26 @@
+
+            dataPlayer = load("winner.mat");
+            %% Make connection to database
+            conn = sqlite('sudoku.db');
+            if dataPlayer.draw == 0
+
+                %Set query to execute on the database
+                query = ['Insert into score (gameMode,winner, timer, loser )' ...
+                    'Values("Duidoku", "' dataPlayer.winner '","' dataPlayer.time '", "' dataPlayer.loser '");' 
+                ];
+            else
+                
+                %Set query to execute on the database
+                query = ['Insert into score (gameMode,winner, timer, loser , draw)' ...
+                    'Values("Duidoku", "' dataPlayer.winner '","' dataPlayer.time '", "' dataPlayer.loser '"  ,"DRAW");' 
+                ];
+
+            end    
+            %% Execute query and fetch results
+            data = fetch(conn,query);
+            
+            %% Close connection to database
+            close(conn)
+            
+            %% Clear variables
+            clear conn query
